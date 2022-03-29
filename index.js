38f402d32778fb76d8ede6495677df7c2cb6fe79
biscrab <biscrab@naver.com>
2022-03-29 23:43:41 +0900

    ☀️ 클러스터 추가

--- index.js
+++ index.js
@@ -191,4 +191,6 @@ app.get('/:user', function(req, res){
 const server = app.listen(process.env.PORT || 5000, () => {
     const port = server.address().port;
     console.log(`Express is working on port ${port}`);
-}, 600000);
+});
+//pm2-runtime index.js
+//"preinstall": "npm install pm2 -g",
